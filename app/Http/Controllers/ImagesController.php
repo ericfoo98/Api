4ad2936c697214eb62d49677f5b8fb52aaddefb9
ericfoo98 <59521344+ericfoo98@users.noreply.github.com>
2020-01-05 21:44:57 +0800

    Update ImagesController.php

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -69,3 +69,44 @@ class ImagesController extends Controller
         return redirect('/api/meme/create');
     }
 }
+
+
+//   $countpage=837;
+//   for($loop=1;$loop<=$countpage;$loop++){
+//
+//     $page="https://interview.agsmartit.com/index.php?page=$loop";
+//     $context=stream_context_create(array('https' => array('timeout' => 10000000000),
+//
+//     ini_set("max_execution_time", 0);
+//     $html = file_get_contents($page,false,$context);
+//
+//     $doc = new \DOMDocument();
+//     @$doc->loadHTML($html);
+//     $xpath = new \DomXPath($doc);
+//     //id
+//     //name
+//     $nodeLists = $xpath->query("//h6[@class='text-center']");
+//     //image path
+//     $tags = $doc->getElementsByTagName('img');
+//
+//
+//     foreach($nodeLists as $nodeList){
+//         $name[] =   $nodeList->nodeValue;
+//         $pages[] =  $loop;
+//
+//     }
+//
+//
+//     foreach ($tags as $tag) {
+//
+//         $url[] =  $tag->getAttribute('src') ;
+//
+//     }
+//
+//     $data = new image();
+//
+//     $data->name = $name[$loop];
+//     $data->url = $url[$loop];
+//     $data->page = $pages[$loop];
+//     $data->save();
+// }
